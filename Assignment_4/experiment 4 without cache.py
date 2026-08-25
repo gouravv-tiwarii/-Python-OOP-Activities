59eb9b703a72105986eaec5fb7047c460d42ced3
gouravv-tiwarii <gouravtiwari7587@gmail.com>
2026-08-25 12:37:52 +0530

    Add files via upload

---
+++ Assignment_4/experiment 4 without cache.py
@@ -0,0 +1,16 @@
+import time
+
+
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n - 1) + fib(n - 2)
+
+
+# Measuring execution time
+start_time = time.perf_counter()
+result = fib(15)
+end_time = time.perf_counter()
+
+print(f"fib(15) = {result}")
+print(f"Execution Time: {(end_time - start_time) * 1000:.6f} ms")
